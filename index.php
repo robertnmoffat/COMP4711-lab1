706d01a3b4bb6c96443c85c8c68c6eaf6df08fa2
Robert <robertnmoffat@gmail.com>
2016-01-09 13:32:58 -0800

    Game logic finished

--- index.php
+++ index.php
@@ -1,16 +1,81 @@
-<?php
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <title>Tic Tac Toe Lab</title>
+    </head>
+    <body>
+        <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-    $name = "Jim";
-    $what = "geek";
-    $level = 10;
-    echo "My level is ".$name.", and I am a level ".$level." ".$what;
-    echo $temp;
-    echo "<br>";
-    $input = $_GET["hours"];
-    echo "You've worked ".$input;
-?>
+        class Game {
+
+            var $position;
+
+            //Constructor for Game class. takes a map of squares as a parameter
+            function __construct($squares) {        
+                $this->position = str_split($squares);                   
+                
+                for ($row = 0; $row < 3; $row++) {
+                    for ($col = 0; $col < 3; $col++) {
+                        echo $this->position[3 * $row + $col];
+                    }
+                    echo "<br>";
+                }
+            }
+
+            //Function to determine if the passed token is the current winner
+            function winner($token) {
+                $won = false;
+
+                //Checking for a line horizontally.
+                for ($row = 0; $row < 3; $row++) {
+                    $result = true;
+                    for ($col = 0; $col < 3; $col++) {
+                        if ($this->position[3 * $row + $col] != $token)
+                            $result = false;
+                    }
+                    if ($result == true) {
+                        $won = true;
+                    }
+                }
+
+                //Checking for a line vertically.
+                for ($col = 0; $col < 3; $col++) {
+                    $result = true;
+                    for ($row = 0; $row < 3; $row++) {
+                        if ($this->position[3 * $row + $col] != $token)
+                            $result = false;
+                    }
+                    if ($result == true) {
+                        $won = true;
+                    }
+                }
+
+                //Checking diagonally, starting with center (if not center, no need to check rest)
+                if ($this->position[4] == $token &&
+                        (($this->position[0] == $token && $this->position[8] == $token) ||
+                        ($this->position[2] == $token && $this->position[6] == $token))) {
+                    $won = true;
+                }
+
+                return $won;
+            }
+
+        }
+
+        if (!isset($_GET['board'])) {
+            echo "No board parameter passed.";
+        } else {
+            $board = $_GET['board'];
+            $game = new Game($board);
+
+            if ($game->winner('x')) {
+                echo "x wins!";
+            } else if ($game->winner('o')) {
+                echo "y wins!";
+            } else {
+                echo "No winner.";
+            }
+        }
+        ?>
+    </body>
+</html>
